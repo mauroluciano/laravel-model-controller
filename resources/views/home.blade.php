34ec805a5f02ecb282bcbfef402f2593be442872
mauroluciano <mauroluciano911@gmail.com>
2023-10-16 17:08:32 +0200

    add forelse

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,4 +4,23 @@
   <section class="container mt-5">
     <h1>{{ $title }}</h1>
   </section>
+
+
+  <div class="row">
+    @forelse ($movies as $movie)
+    <div class="col-3">
+        <div class="card h-100">
+            <div class="card-body text-center">
+                <h3>{{ $movie['title'] }}</h3>
+                <p>{{ $movie['nationality'] }}</p>
+            </div>    
+            </div>
+    </div>
+        
+    @empty
+        no Comics
+    @endforelse
+</div>
+
+@endsection
 @endsection
